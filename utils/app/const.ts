c9289eb65d2ad6ebdaf6015d7c1d40078f50ca98
Jeongkyu Shin <inureyes@gmail.com>
2023-05-24 12:58:01 +0900

    update: prompt

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,7 @@
 export const DEFAULT_SYSTEM_PROMPT =
-  process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT || 
+  "You are LablupGPT, a large language model trained by OpenAI and fine-tuned by Lablup. Follow the user's instructions carefully. Respond using markdown.";
+
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
